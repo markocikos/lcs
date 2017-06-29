a22b46235225191d2fb93ebd25dfef9a8f7122a9
Marko Čikoš <marko.cikos@liferay.com>
2017-06-29 10:42:44 +0200

    Remove switches that only make sense when the server is up - script should be ran only when the server is down

--- scripts/buildlcs.sh
+++ scripts/buildlcs.sh
@@ -11,48 +11,46 @@ deployTomcatHome=$deployRoot"/tomcat-8.0.32"
 libsPath=$(pwd)"/lib"
 
 ### CLEAN ###
-if [[ "$1" == *"c"* ]] ; then
-	if [ -d $deployRoot"/deploy" ] ; then
-		echo "Deleting /deploy"
-		cd $deployRoot"/deploy"
-		exec & rm -rf *
-	fi
+if [ -d $deployRoot"/deploy" ] ; then
+	echo "Deleting /deploy"
+	cd $deployRoot"/deploy"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployRoot"/osgi/modules" ] ; then
-		echo "Deleting /osgi/modules"
-		cd $deployRoot"/osgi/modules"
-		exec & rm -rf *
-	fi
+if [ -d $deployRoot"/osgi/modules" ] ; then
+	echo "Deleting /osgi/modules"
+	cd $deployRoot"/osgi/modules"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployRoot"/osgi/state" ] ; then
-		echo "Deleting /osgi/state"
-		cd $deployRoot"/osgi/state"
-		exec & rm -rf *
-	fi
+if [ -d $deployRoot"/osgi/state" ] ; then
+	echo "Deleting /osgi/state"
+	cd $deployRoot"/osgi/state"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployRoot"/osgi/war" ] ; then
-		echo "Deleting /osgi/war"
-		cd $deployRoot"/osgi/war"
-		exec & rm -rf *
-	fi
+if [ -d $deployRoot"/osgi/war" ] ; then
+	echo "Deleting /osgi/war"
+	cd $deployRoot"/osgi/war"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployTomcatHome"/temp" ] ; then
-		echo "Deleting Tomcat temp"
-		cd $deployTomcatHome"/temp"
-		exec & rm -rf *
-	fi
+if [ -d $deployTomcatHome"/temp" ] ; then
+	echo "Deleting Tomcat temp"
+	cd $deployTomcatHome"/temp"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployTomcatHome"/work" ] ; then
-		echo "Deleting Tomcat work"
-		cd $deployTomcatHome"/work"
-		exec & rm -rf *
-	fi
+if [ -d $deployTomcatHome"/work" ] ; then
+	echo "Deleting Tomcat work"
+	cd $deployTomcatHome"/work"
+	exec & rm -rf *
+fi
 
-	if [ -d $deployRoot"/work" ] ; then
-		echo "Deleting /work"
-		cd $deployRoot"/work"
-		exec & rm -rf *
-	fi
+if [ -d $deployRoot"/work" ] ; then
+	echo "Deleting /work"
+	cd $deployRoot"/work"
+	exec & rm -rf *
 fi
 
 ### MYSQL ###
@@ -140,12 +138,3 @@ exec & gradle deploy
 
 cd $gitPortalRoot"/apps/foundation/petra/petra-json-web-service-client"
 exec & gradle deploy
-
-### DIAGNOSE ERRORS ###
-if [[ "$1" == *"s"* ]] ; then
-	## show status of all modules
-	exec & blade sh ss | grep lcs
-
-	## show if there are any dependencies issues
-	exec & blade sh dm | grep unavailable
-fi
