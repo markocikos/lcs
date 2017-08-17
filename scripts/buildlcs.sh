4f01f9b35d421e4c60a05965d724122cc9189d24
Marko Čikoš <marko.cikos@liferay.com>
2017-08-17 12:03:08 +0200

    Replace individual gradle builds with all-module tasks

--- scripts/buildlcs.sh
+++ scripts/buildlcs.sh
@@ -3,8 +3,6 @@
 ### PATHS ###
 deployRoot="/d/liferay/portal/dxp-7.0"
 gitLCSRoot="/d/git/lcs"
-gitMonitorRoot="/d/git/internship/team-assignments/monitor"
-gitPortalRoot="/d/git/ee-7.0/portal/modules"
 
 deployTomcatHome=$deployRoot"/tomcat-8.0.32"
 
@@ -63,84 +61,9 @@ if [ ! -f $deployTomcatHome"/lib/ext/mysql.jar" ] ; then
 	fi
 fi
 
-### OSB LCS MODULES ###
-cd $gitLCSRoot"/osb-lcs-admin-web"
-exec & gradle deploy
+### LCS MODULES ###
+cd $gitLCSRoot
 
-cd $gitLCSRoot"/osb-lcs-api"
-exec & gradle deploy
+exec & gradle deployLCSModules
 
-cd $gitLCSRoot"/osb-lcs-cache"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-keystore"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-lang"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-nosql"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-nosql-api"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-nosql-service"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-nosql-service-activator"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-queue"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-report"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-service"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-taglib"
-exec & gradle deploy
-
-cd $gitLCSRoot"/osb-lcs-theme"
-exec & gulp deploy
-
-cd $gitLCSRoot"/osb-lcs-web"
-exec & gradle deploy
-
-### MONITOR MODULES ####
-cd $gitMonitorRoot"/monitor-singular-web"
-exec & gradle deploy
-
-cd $gitMonitorRoot"/monitor-theme"
-exec & gulp deploy
-
-### PORTAL MODULES ####
-privateApps=$gitPortalRoot"/private/apps"
-
-oauth=$privateApps"/oauth"
-
-cd $oauth"/oauth-api"
-exec & gradle deploy
-
-cd $oauth"/oauth-service"
-exec & gradle deploy
-
-lcs=$privateApps"/lcs"
-
-cd $lcs"/lcs-messaging"
-exec & gradle deploy
-
-cd $lcs"/lcs-portlet"
-exec & gradle deploy
-
-cd $lcs"/lcs-security"
-exec & gradle deploy
-
-## lcs-api and petra should be built last, to override deployed modules from dependency definitions in build.gradles. Awesome!
-cd $lcs"/lcs-api"
-exec & gradle deploy
-
-cd $gitPortalRoot"/apps/foundation/petra/petra-json-web-service-client"
-exec & gradle deploy
+exec & gradle deployOSBLCSModules
